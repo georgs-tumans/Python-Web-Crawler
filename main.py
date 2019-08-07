badbbcc468e38ad6a18c175f1d730e90a2512e98
Georgs <georgs.tumans@zzdats.lv>
2019-08-07 11:50:52 +0300

    First actual function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ('Hello!')
+import scrapy
+import logging
+
+logging.getLogger('scrapy').setLevel(logging.WARNING)
+class main (scrapy.Spider):
+    name = 'Wikipedia'
+    start_urls = ['https://en.wikipedia.org/wiki/Battery_(electricity)']
+
+    def parse(self, response):
+        print (response.css('h1#firstHeading::text').extract())
