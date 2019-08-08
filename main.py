41a5200462a23e1f31c7e0fd3342d82f8cf60216
Georgs <georgs.tumans@zzdats.lv>
2019-08-08 12:03:16 +0300

    Crawler now can infinitely visit all links found on pages+ writes visited wiki pages' titles to file

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import scrapy
 import logging
 
-# Palaist crawleri - scrapy runspider QuotesSpider.py
+# Palaist crawleri - scrapy runspider main.py
+# Palaist crawleri un ssaglabāt datus json failā - scrapy runspider main.py
 
 logging.getLogger('scrapy').setLevel(logging.WARNING)
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-
-    
+    name = "quotes" 
     start_urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'https://en.wikipedia.org/wiki/Main_Page'
     ]
-        
+    count=0 
+    with open("wiki.txt", 'a+') as f:
+        f.write("The visited wikipedia pages: "+"\r\n")    
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        if "wikipedia" in response.url:
+            heading = response.css("h1#firstHeading::text").get()
+            if heading is not None:
+                with open("wiki.txt", 'a+') as f:
+                    f.write(heading+"\r\n")
+            
+        self.count+=1
+        print ('Currently visiting link nr ' + str(self.count) + ': '+ response.url)
+        for href in response.css("a::attr(href)").getall():
+            yield response.follow(href, callback=self.parse)    #response.follow ir ideāls veids kā ielikt rindā jaunu linku, jo tas saprot arī relatīvos url
